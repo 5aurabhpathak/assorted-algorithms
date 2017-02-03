9350f70dca1459e680dbe158e86235bf75dcf94b
5aurabhpathak <thephoenix97@gmail.com>
2017-02-03 23:20:23 +0530

    submodule precommit

--- python/nlp/mtech-thesis/prepare/clean_corpora.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Author: Saurabh Pathak
-#Third Step
-#cleaning of parallel corpus - uses (my own) python scripts.
-#monolingual corpus was not cleaned. However, certain literatures suggest that such a cleaning might benefit the translation system. (Ref. topic 'Controlled Language rules in MT'). I am not much inclined towards it as of now because of the nature and proportion of my work. Furthermore, we also have the post edition option to correct any errors introduced by noise in the corpus.
-$HOME/src/python/nlp/mtech-thesis/prepare/clean_bilingual_corpus.py
-exit 0
-#future: may add cleaning of monolingual corpora if necessary.
-#My hypothesis at this point is that it shouldn't be.
