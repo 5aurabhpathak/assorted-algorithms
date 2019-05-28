2946435d10cbac66848e86ca6eb552164e4732b2
5aurabhpathak <s.pathak@ieee.org>
2019-05-28 22:01:53 +0530

    tidy up

--- python/batman/classifier.py
+++
@@ -1,8 +0,0 @@
-from threading import Event
-from abc import ABC, abstractmethod
-
-class Classifier(ABC):
-    def __init__(self, exchange): self.predicted, self.exchange, self.predict_sp, self.predict_bp = Event(), exchange, None, None
-    
-    @abstractmethod
-    def predict(self): pass
